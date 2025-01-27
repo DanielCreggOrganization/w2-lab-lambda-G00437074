a2ceaac4565e0038c986d4db5969de5466efb1b4
Adam Devlin <145337473+G00437074@users.noreply.github.com>
2025-01-27 13:18:37 +0000

    Completing Example 1

---
+++ src/ie/atu/lambda/Main.java
@@ -0,0 +1,8 @@
+package ie.atu.lambda;
+
+public class Main {
+    public static void main(String[] args) {
+        MathOperation addition = new Addition();
+        System.out.println("Addition: " + addition.operate(5, 3));
+    }
+}
