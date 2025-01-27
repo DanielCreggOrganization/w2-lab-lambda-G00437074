41c1a15aa87bea297dd76fdc993b9c210819b447
Adam Devlin <145337473+G00437074@users.noreply.github.com>
2025-01-27 13:42:33 +0000

    Updating Part 1 DIY

--- src/ie/atu/lambda/Main.java
+++ src/ie/atu/lambda/Main.java
@@ -2,21 +2,24 @@ package ie.atu.lambda;
 
 public class Main {
     public static void main(String[] args) {
-    // Part 1:
-        // Example:
+    // Part 1 Example:
         MathOperation addition = new Addition();
         System.out.println("Addition: " + addition.operate(5, 3));
 
-        // Create an instance of ToUpperCase
-        StringOperation toUpperCase = new ToUpperCase();
+    // Part 1 DIY:
+         // Use a lambda expression to implement the StringOperation interface
+         StringOperation toUpperCase = input -> input.toUpperCase();
 
-        // Test the apply method with a string of your choice
-        String input = "hello, world!";
-        String result = toUpperCase.apply(input);
+         // Test the apply method with a string of your choice
+         String input = "my name is adam!";
+         String result = toUpperCase.apply(input);
+ 
+         // Print the result
+         System.out.println("Input: " + input);
+         System.out.println("Result: " + result);
 
-        // Print the result
-        System.out.println("Input: " + input);
-        System.out.println("Result: " + result);
+    // Part 2 DIY:
 
-    }
-}
+
+     }
+ }
